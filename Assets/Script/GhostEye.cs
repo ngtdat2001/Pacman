ee7da1e934333ad3efe4fda5c611eb002cf7c8c3
ngtdat2001 <nero2112001@gmail.com>
2023-04-17 13:28:15 +0700

    Fix bug

--- Assets/Script/GhostEye.cs
+++ Assets/Script/GhostEye.cs
@@ -1,37 +1,40 @@
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class GhostEye : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer { get; private set; }
-    public Movement movement { get; private set; }
     public Sprite up;
     public Sprite down;
     public Sprite left;
     public Sprite right;
 
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Movement movement { get; private set; }
+
     private void Awake()
     {
-        this.spriteRenderer = GetComponent<SpriteRenderer>();
-        this.movement = GetComponent<Movement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        movement = GetComponentInParent<Movement>();
     }
 
     private void Update()
     {
-        if(this.movement.direction == Vector2.up)
+        if (movement.direction == Vector2.up)
         {
-            this.spriteRenderer.sprite = up;
+            spriteRenderer.sprite = up;
         }
-        if (this.movement.direction == Vector2.down)
+        else if (movement.direction == Vector2.down)
         {
-            this.spriteRenderer.sprite = down;
+            spriteRenderer.sprite = down;
         }
-        if (this.movement.direction == Vector2.left)
+        else if (movement.direction == Vector2.left)
         {
-            this.spriteRenderer.sprite = left;
+            spriteRenderer.sprite = left;
         }
-        if (this.movement.direction == Vector2.right)
+        else if (movement.direction == Vector2.right)
         {
-            this.spriteRenderer.sprite = right;
+            spriteRenderer.sprite = right;
         }
     }
+
 }
